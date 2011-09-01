46c46ef66a2c495cc4b29a651d2a4cc215e5d0b6
Csaba Nemeth <cnemeth@hedgeye.com>
2011-09-01 15:01:08 -0700

    edited session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Bcmsheroku::Application.config.session_store :cookie_store, :key => '_bcmsheroku_session'
+# Try commenting this out
+# Bcmsheroku::Application.config.session_store :cookie_store, :key => '_bcmsheroku_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
