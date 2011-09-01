328b8074a2c181f6b93613247e2f957f86932481
Csaba Nemeth <cnemeth@hedgeye.com>
2011-09-01 15:29:16 -0700

    disabled cashing in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,16 +3,13 @@ Bcmsheroku::Application.configure do
   config.action_controller.page_cache_directory = Rails.root + '/public/cache/'
   # Settings specified here will take precedence over those in config/application.rb
 
-  # Test this setting
-  config.perform_caching = true
-
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
