f18fbb1cb27b742b712e35bfcc73ed00f133bafd
Csaba Nemeth <cnemeth@hedgeye.com>
2011-09-01 09:03:04 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bcmsheroku::Application.config.secret_token = 'b2b855522086dd99ff7150758fd02251614db72ddae811a8f5431267624dee313404b2c37615e019fdf8744656594a40ed4a8137d90ed9fb7ac01292fa27fe9c'
