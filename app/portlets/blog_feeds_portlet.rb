4e08e2a1d3a406921b16353451c877c88d0f0507
Csaba Nemeth <cnemeth@hedgeye.com>
2011-09-07 16:38:48 -0700

    fixed error in sort_by_publish_date method  of blog_feeds_portlet.rb

--- app/portlets/blog_feeds_portlet.rb
+++ app/portlets/blog_feeds_portlet.rb
@@ -32,8 +32,7 @@ private
   end
 
   def sort_by_publish_date(entries)
-   # entries.sort{|a,b| b.published <=> a.published}
-   entries
+   entries.sort{ |a,b| b.published.to_i <=> a.published.to_i }
   end
 
 end
