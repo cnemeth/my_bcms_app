956453c2ed12747e45785004eb7c32694fa9b118
Csaba Nemeth <cnemeth@hedgeye.com>
2011-09-12 15:47:39 -0700

    converted blog entry summary and content to string and called html_safe on them in the render.html.erb of the blog feeds portlet, also uncommented the sanitize_entries line in the controller

--- app/portlets/blog_feeds_portlet.rb
+++ app/portlets/blog_feeds_portlet.rb
@@ -23,7 +23,7 @@ private
   def sanitize_and_interleave(feeds)
     entries = []
     feeds.each do | key, value |
-      #value.sanitize_entries!
+      value.sanitize_entries!
       value.entries.each do | e |
         entries << e
       end
