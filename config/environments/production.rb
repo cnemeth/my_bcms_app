4b6788829b344c5096ca4ff52863b651e27be1d6
Csaba Nemeth <cnemeth9@gmail.com>
2011-09-14 11:28:36 -0700

    added config.action_mailer.default_url_options = { ... } to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,7 @@ Bcmsheroku::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'http://blazing-frost-254.herokuapp.com' }
 
   # Enable threaded mode
   # config.threadsafe!
