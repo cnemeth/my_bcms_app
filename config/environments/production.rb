9a32dae35232d42b5ecec6de1f13969165172049
Csaba Nemeth <cnemeth9@gmail.com>
2011-09-15 11:58:29 -0700

    added smtp and ActionMailer configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,9 +37,8 @@ Bcmsheroku::Application.configure do
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
-  # config.action_mailer.default_url_options = { :host => 'http://blazing-frost-254.herokuapp.com' }
+  # Enable email delivery errors
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable threaded mode
   # config.threadsafe!
