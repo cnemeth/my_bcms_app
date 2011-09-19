c0bd25c00da7b82ab4e58763205f2dc7e0561b7f
Csaba Nemeth <cnemeth9@gmail.com>
2011-09-19 11:49:42 -0700

    fixed sessin store with dalli, added cucumber feature for blog aggregator

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Try commenting this out
-#Bcmsheroku::Application.config.session_store :cookie_store, :key => '_bcmsheroku_session'
+# Bcmsheroku::Application.config.session_store :cookie_store, :key => '_bcmsheroku_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
@@ -16,3 +16,5 @@ ActionController::Base.session = {
 
 # require 'action_controller/session/dalli_store' 
 # ActionController::Base.session_store = :dalli_store 
+require 'action_dispatch/middleware/session/dalli_store' 
+Rails.application.config.session_store :dalli_store
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100705083859) do
+ActiveRecord::Schema.define(:version => 20110919181858) do
 
   create_table "attachment_versions", :force => true do |t|
     t.integer  "attachment_id"
@@ -414,6 +414,16 @@ ActiveRecord::Schema.define(:version => 20100705083859) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "sites", :force => true do |t|
     t.string   "name"
