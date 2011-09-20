e1fd7a8912b298ea6d76a532273a275624a38e5f
Scott Smith <ssmith@hedgeye.com>
2011-09-20 16:53:50 -0700

    Fixed spec file to use reasonable and customary 'require' expressions

--- spec/blog_aggregator/blogs_spec.rb
+++ spec/blog_aggregator/blogs_spec.rb
@@ -1,21 +1,19 @@
-temp_path = "~/work/engineeringsite-bcms/spec/spec_helper"
-puts temp_path
-require temp_path
+require File.expand_path("../../spec_helper", __FILE__)
 
-describe "Blog Aggregrator" do
+describe BlogFeedsPortlet do
 
   before(:each) do
     @bp = BlogFeedsPortlet.new
   end
 
   it "keeps the aggregated logs in reverse chronological order" do
-    @entries = @bp.render
-    @entries.should_not be nil
-    @entries.is_a?(Fixnum).should be false
-    @entries.count.should > 0
-    0.upto(@entries.count - 2) do | offset |
-        latest_entry = @entries[offset]
-        previous_entry = @entries[offset + 1]
+    entries = @bp.render
+    entries.should_not be nil
+    entries.is_a?(Fixnum).should be false
+    entries.count.should > 0
+    0.upto(entries.count - 2) do | offset |
+        latest_entry = entries[offset]
+        previous_entry = entries[offset + 1]
         latest_entry.published.should >= previous_entry.published
     end
   end
